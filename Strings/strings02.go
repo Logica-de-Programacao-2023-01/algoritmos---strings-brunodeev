b2c2ba41db4b4c9ec687ec253bc6a4e0666eafd4
brunodeev <bruno.meireles@sempreceub.com>
2023-04-03 17:56:09 -0300

    String 02

--- Strings/strings02.go
+++ Strings/strings02.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	word := "T e s t e"
+
+	newWord := strings.ReplaceAll(word, " ", "")
+
+	fmt.Printf("A string sem espaços será: %s", newWord)
 }
